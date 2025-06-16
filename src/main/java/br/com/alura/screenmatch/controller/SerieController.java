71f19320e56fe051caddb1e0c10d558a2f35f0b4
MarlonVieira <marlongabriel_100@hotmail.com>
2025-06-16 20:51:46 -0300

    03. Mapping the Screenmatch home page. 06/16/2025

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,7 +1,7 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
-import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +11,12 @@ import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
+
     @Autowired
-    private SerieRepository repository;
+    private SerieService service;
 
     @GetMapping("/series")
     public List<SerieDTO> getSeries() {
-        return repository.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitle(), s.getTotalSeasons(), s.getRating(), s.getYear(), s.getGenre(), s.getActors(), s.getPoster(), s.getSynopsis()))
-                .collect(Collectors.toList());
+        return service.getAllSeries();
     }
 }
--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,8 +1,22 @@
 package br.com.alura.screenmatch.service;
 
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SerieService {
-    
+    @Autowired
+    private SerieRepository repository;
+
+    public List<SerieDTO> getAllSeries() {
+        return repository.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitle(), s.getTotalSeasons(), s.getRating(), s.getYear(), s.getGenre(), s.getActors(), s.getPoster(), s.getSynopsis()))
+                .collect(Collectors.toList());
+    }
 }
